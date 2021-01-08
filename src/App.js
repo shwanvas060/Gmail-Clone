6e2a085df77d71e88c021142a918ed638b524200
shwanvas060 <2019202060@annauniv.edu.in>
2021-01-08 08:10:20 +0530

    SideBar Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar />
-  
-      <h3>redux gmail clone</h3>
+      
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,64 @@
-import {Button} from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
-
-function Sidebar(){
-    return(<div className="sidebar">
-        <Button startIcon={<AddIcon fontSize="large"  />}className="sidebar_compose" >
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar_compose"
+      >
         compose
-        </Button>
-        <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
-    </div>
-    );
+      </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={false}
+      />
+      <SidebarOption
+        Icon={StarIcon}
+        title="Starred"
+        number={2}
+        selected={true}
+      />
+      <SidebarOption
+        Icon={AccessTimeIcon}
+        title="Snoozed"
+        number={55}
+        selected={true}
+      />
+      <SidebarOption Icon={LabelImportantIcon} title="important" number={55} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={44} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" />
 
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Sidebar;
